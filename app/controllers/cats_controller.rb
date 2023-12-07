ec7cfd34ac2046decb90c6463d4fd136949d1a1d
triciasykes <tlsykes98@gmail.com>
2023-12-07 10:20:12 -0800

    completed api endpoints

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,28 @@
 class CatsController < ApplicationController
+
+  def index
+    cats = Cat.all 
+    render json: cats
+  end
+
+  def create
+    cat = Cat.create(cat_params)
+    render json: cat
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+    cat.update(cat_params)
+    render json: cat
+  end
+
+  def destroy
+  end
+
+  private
+  #strong params
+  def cat_params
+    params.require(:cat).permit(:name,:age, :enjoys, :image)
+  end
+
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,82 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a list of cats' do
+      Cat.create(
+        name: 'Felix',
+        age: 2,
+        enjoys: 'Long walks on the beach at sunset',
+        image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+      )
+
+       get '/cats'
+       cat = JSON.parse(response.body)
+
+      #checking we have successful request /status 200
+       expect(response).to have_http_status(200)
+       # making sure we are returning accurate amount of data
+       expect(cat.length).to eq 1
+
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a cat" do
+      #define the params going to send with request
+      cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 4,
+          enjoys: 'Meow Mix and breakdancing',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      # Send request to the server and pass params needed for create
+      post '/cats', params: cat_params
+
+      # check for successful response
+      expect(response).to have_http_status(200)
+
+      cat = Cat.first
+      # Assure created cat has correct attributes
+      expect(cat.name).to eq 'Felix'
+      expect(cat.age).to eq 4
+      expect(cat.enjoys).to eq 'Meow Mix and breakdancing'
+      expect(cat.image).to eq 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+    end
+  end 
+
+  describe "PATCH /update" do
+    it "updates a cat" do
+
+      # create a cat
+      cat_params = {
+        cat: {
+          name: 'Felix',
+          age: 4,
+          enjoys: 'Meow Mix and breakdancing',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+      p cat
+
+      # update the cat
+      updated_params = {
+        cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys:'Meow Mix and breakdancing',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+
+      patch "/cats/#{cat.id}", params: updated_params
+
+      updated_cat = Cat.find(cat.id)
+      expect(response).to have_http_status 200
+      expect(updated_cat.name).to eq "Buster"
+    end
   end
 end
