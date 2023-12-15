b8347e6ab4f64437aec1ae300da4287245145aa1
triciasykes <tlsykes98@gmail.com>
2023-12-15 09:53:17 -0800

    updated url in cors for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # <- change this to allow requests from any domain while in development.
+    origins 'cat-tinder-frontend.onrender.com'  # <- change this to allow requests from any domain while in development.
 
     resource '*',
       headers: :any,
